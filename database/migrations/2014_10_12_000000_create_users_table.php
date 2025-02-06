fd237a7e4a823c07bfeab5a9862ba27c5d0cea3b
Zapupa <vitaliy2005rus@gmail.com>
2025-02-06 08:43:29 +0500

    Васильев В.А. htfkbpjdfyf ,fpf lfyys[ cj cdzpzvb

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -14,12 +17,22 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('tel');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => "adminker",
+            'tel' => "999999999",
+            'email' => "admin@mail.com",
+            'password' => Hash::make('admin1234'),
+            'role' => "admin"
+        ]);
     }
 
     /**
